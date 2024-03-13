c6c7122ec6e772c489625c3ecb5d5efcbb40b7f5
choeyeji <cyj000424@naver.com>
2024-03-13 15:29:06 +0900

    feat : 좋아요 기능 v1 구현 (#15)

--- src/main/java/com/study/footprint/controller/like/LikeController.java
+++ src/main/java/com/study/footprint/controller/like/LikeController.java
@@ -1,8 +1,15 @@
 package com.study.footprint.controller.like;
 
+import com.study.footprint.common.response.SingleResult;
+import com.study.footprint.config.ConfigUtil;
+import com.study.footprint.dto.like.response.ClickLikeResDto;
 import com.study.footprint.service.like.LikeService;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -10,9 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikeController {
 
     private final LikeService likeService;
+    private final ConfigUtil configUtil;
 
     @Builder
-    public LikeController(LikeService likeService) {
+    public LikeController(LikeService likeService, ConfigUtil configUtil) {
         this.likeService = likeService;
+        this.configUtil = configUtil;
+    }
+
+    /**
+     * 좋아요가 안 눌려있으면 취소, 눌려있다면 등록
+     * @param postingId
+     * @return SingleResult<ClickLikeResDto> isLike(좋아요 상태) (true, false)
+     */
+    @PostMapping("/v1/like/{posting-id}")
+    public ResponseEntity<SingleResult<ClickLikeResDto>> clickLike(@PathVariable("posting-id") Long postingId) {
+
+        Long userId = configUtil.getLoginUserId();
+
+        SingleResult<ClickLikeResDto> result = likeService.clickLike(postingId, userId);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
